1895e4caf47d630ee8d9e7432e72ef2c4e9d7a11
Gabriel Monroy <monroygabriel9@gmail.com>
2024-05-14 22:46:37 -0600

    se modifico la clase Incident para aceptar los JOINS cambiaron los tipos de datos de Long a String

--- src/main/java/src/javasqlriskmanager/models/Incident.java
+++ src/main/java/src/javasqlriskmanager/models/Incident.java
@@ -4,7 +4,19 @@ import java.sql.Date;
 
 public class Incident {
 
-    public Incident(String title, Long id, String description, Date createdAt, Date updateDate, Long id_status, Long id_severity, Long id_creatorUser, Long id_assignedUser, Long id_department) {
+    String title;
+    Long id;
+    String description;
+    Date createdAt;
+    Date updateDate;
+    String id_status;
+    String id_severity;
+    String id_department;
+
+    public Incident() {
+    }
+
+    public Incident(String title, Long id, String description, Date createdAt, Date updateDate, String id_status, String id_severity, String id_department) {
         this.title = title;
         this.id = id;
         this.description = description;
@@ -12,21 +24,8 @@ public class Incident {
         this.updateDate = updateDate;
         this.id_status = id_status;
         this.id_severity = id_severity;
-        this.id_creatorUser = id_creatorUser;
-        this.id_assignedUser = id_assignedUser;
         this.id_department = id_department;
     }
-
-    String title;
-    Long id;
-    String description;
-    Date createdAt;
-    Date updateDate;
-    Long id_status;
-    Long id_severity;
-    Long id_creatorUser;
-    Long id_assignedUser;
-    Long id_department;
 
     public String getTitle() {
         return title;
@@ -68,43 +67,27 @@ public class Incident {
         this.updateDate = updateDate;
     }
 
-    public Long getId_status() {
+    public String getId_status() {
         return id_status;
     }
 
-    public void setId_status(Long id_status) {
+    public void setId_status(String id_status) {
         this.id_status = id_status;
     }
 
-    public Long getId_severity() {
+    public String getId_severity() {
         return id_severity;
     }
 
-    public void setId_severity(Long id_severity) {
+    public void setId_severity(String id_severity) {
         this.id_severity = id_severity;
     }
 
-    public Long getId_creatorUser() {
-        return id_creatorUser;
-    }
-
-    public void setId_creatorUser(Long id_creatorUser) {
-        this.id_creatorUser = id_creatorUser;
-    }
-
-    public Long getId_assignedUser() {
-        return id_assignedUser;
-    }
-
-    public void setId_assignedUser(Long id_assignedUser) {
-        this.id_assignedUser = id_assignedUser;
-    }
-
-    public Long getId_department() {
+    public String getId_department() {
         return id_department;
     }
 
-    public void setId_department(Long id_department) {
+    public void setId_department(String id_department) {
         this.id_department = id_department;
     }
 
@@ -118,8 +101,6 @@ public class Incident {
                 ", updateDate=" + updateDate +
                 ", id_status=" + id_status +
                 ", id_severity=" + id_severity +
-                ", id_creatorUser=" + id_creatorUser +
-                ", id_assignedUser=" + id_assignedUser +
                 ", id_department=" + id_department +
                 '}';
     }
