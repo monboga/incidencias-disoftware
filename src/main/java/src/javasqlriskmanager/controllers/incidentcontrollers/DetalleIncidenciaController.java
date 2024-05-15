937ed02b4f80e3d06c20de2f34c1cb254002f69a
Hypeyiii <144419751+Hypeyiii@users.noreply.github.com>
2024-05-14 20:37:16 -0600

    Error de sintaxis

--- src/main/java/src/javasqlriskmanager/controllers/incidentcontrollers/DetalleIncidenciaController.java
+++ src/main/java/src/javasqlriskmanager/controllers/incidentcontrollers/DetalleIncidenciaController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Scene;;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import org.w3c.dom.Text;
 import src.javasqlriskmanager.MainApplication;
